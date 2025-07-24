db92fdf2a845df434832d0be12700b2eaf441475
Alysson Demari <alyssondemari@gmail.com>
2025-07-24 16:56:33 -0300

    organizando o header

--- js/scroll.js
+++ js/scroll.js
@@ -8,19 +8,18 @@ const hamburger = document.querySelector('#hamburger');
 const mobileNav = document.querySelector('.header__ul-mobile');
 const bars = document.querySelectorAll('.bar1, .bar2, .bar3');
 
-// Função para ABRIR e FECHAR o menu ao clicar no ícone
+// Abre o navBar
 hamburger.addEventListener('click', () => {
   mobileNav.classList.toggle('active');
-  // Itera sobre as barras para adicionar/remover a classe de uma vez
   bars.forEach(bar => bar.classList.toggle('active'));
 });
 
-// Listener para FECHAR o menu ao clicar fora dele
+// fecha o navBar ao clicar fora dele
 document.addEventListener('click', (event) => {
-  // Verifica se a classe 'active' está presente (ou seja, se o menu está aberto)
+  // verifica se o navBar esta ativo
   const isMenuOpen = mobileNav.classList.contains('active');
   
-  // Verifica se o alvo do clique NÃO está dentro do menu e NÃO é o próprio ícone do hamburguer
+  // verifica se o click não foi no navBar
   const isClickOutside = !mobileNav.contains(event.target) && !hamburger.contains(event.target);
 
   if (isMenuOpen && isClickOutside) {
