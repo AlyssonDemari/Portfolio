3922e203f47f12b7a7131e5f9e44be478ae31454
Alysson Demari <alyssondemari@gmail.com>
2025-07-24 12:43:59 -0300

    modificando a estrutura

--- js/scroll.js
+++ js/scroll.js
@@ -4,13 +4,13 @@ window.addEventListener("scroll", function(){
 })
 
 
-        const hamburger = document.querySelector('#hamburger');
-        const bar1 = document.querySelector('.bar1');
-        const bar2 = document.querySelector('.bar2');
-        const bar3 = document.querySelector('.bar3');   
+const hamburger = document.querySelector('#hamburger');
+const bar1 = document.querySelector('.bar1');
+const bar2 = document.querySelector('.bar2');
+const bar3 = document.querySelector('.bar3');   
 
-        hamburger.addEventListener('click', () => {
-            bar1.classList.toggle('active');
-            bar2.classList.toggle('active');
-            bar3.classList.toggle('active');
-        })
+hamburger.addEventListener('click', () => {
+    bar1.classList.toggle('active');
+    bar2.classList.toggle('active');
+    bar3.classList.toggle('active');
+})
